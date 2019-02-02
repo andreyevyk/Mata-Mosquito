8de7b3406152e137811d6c396f2325fe2fd8cb44
Andrey Evyk <andreyevyk13@gmail.com>
2019-02-02 01:16:15 -0300

    versão 2 inicializada

--- jogo.js
+++ jogo.js
@@ -2,7 +2,31 @@
 
 var altura = 0;
 var largura = 0;
-var vidas = 3;
+var vidas = 1;
+var tempo = 1;
+var pontuacao = 0;
+
+var MoscaTempo = 2000; 
+
+function criarMoscaTempo(){
+   if(pontuacao>=50 && pontuacao<100){
+      MoscaTempo = 1800;
+   }
+   else if(pontuacao>=100 && pontuacao<150){
+      MoscaTempo = 1500;
+   }
+   else if(pontuacao>=150 && pontuacao<200){
+      MoscaTempo = 1000;
+   }
+   else if(pontuacao>=200 && pontuacao<250){
+      MoscaTempo = 750;
+   }
+   else if(pontuacao>=250){
+      MoscaTempo = 500;
+   }
+   return MoscaTempo;
+}
+
 
 function ajustaTamanhoPalcoJogo(){
    altura = window.innerHeight;
@@ -10,17 +34,23 @@ function ajustaTamanhoPalcoJogo(){
 }
 ajustaTamanhoPalcoJogo();
 
+var cronometro = setInterval(function(){
+
+   tempo +=1;
+
+   document.getElementById('cronometro').innerHTML = tempo;
+},1000)
+
 function posicaoRandomica(){
 
    if(document.getElementById('mosquito')){
       document.getElementById('mosquito').remove();
       
-      console.log(vidas)
-      if(vidas==0){
-         alert('perdeu')
+      if(vidas>3){
+         window.location.href = 'fimDeJogo.html?'+pontuacao;
       }else{
          document.getElementById('v'+vidas).src = "img/coracao_vazio.png"
-         vidas--
+         vidas++;
       }
       
    }
@@ -28,7 +58,7 @@ function posicaoRandomica(){
    var posicaoX = Math.random()*largura;
    posicaoX = Math.floor(posicaoX) - 90;
    var posicaoY = Math.random()*altura;
-   posicaoY = (Math.ceil(posicaoY) - 90)-document.getElementById('painel').clientHeight;
+   posicaoY = (Math.floor(posicaoY) - 90)-document.getElementById('painel').clientHeight;
 
    if(posicaoX<0){
       posicaoX = 0;
@@ -36,7 +66,6 @@ function posicaoRandomica(){
    else if(posicaoY<0){
       posicaoY = 0;
    }
-   console.log(posicaoX,posicaoY) ;
 
    var mosquito  = document.createElement('img');
    mosquito.src = 'img/mosca.png';
@@ -48,6 +77,15 @@ function posicaoRandomica(){
    mosquito.id = 'mosquito'
    mosquito.onclick = function(){
       this.remove();
+      if(mosquito.className.lastIndexOf('mosquito1') !=-1 ){
+         pontuacao+=5;
+      }else if(mosquito.className.lastIndexOf('mosquito2') !=-1 ){
+         pontuacao+=3;
+      }else if(mosquito.className.lastIndexOf('mosquito3') !=-1 ){
+         pontuacao+=1;
+      }
+      document.getElementById('pont').innerHTML = pontuacao;
+      
    }
    document.body.appendChild(mosquito);
    
